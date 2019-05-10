52c4d2a04c6db759be9b6655a109f57f0b998484
mmt09 <michaelmcdonaldtran@gmail.com>
2019-05-10 16:34:51 -0700

    added all tables

--- display.php
+++ display.php
@@ -9,15 +9,86 @@ if (!$conn) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());	
 }
 
-$sql = 'SELECT first_name, last_name, certification FROM trainer';
-		
-$query = mysqli_query($conn, $sql);
+if(isset($_POST['formClass']))
+{
+	$formClass = $_POST['formclass'];
+	$Aquatics = (isset($_POST['Aquatics']) ? $_POST['Aquatics']: null);
+	$Boxing = (isset($_POST['Boxing']) ? $_POST['Boxing']: null);
+	$Cycling = (isset($_POST['Cycling']) ? $_POST['Cycling']: null);
+	$Dance = (isset($_POST['Dance']) ? $_POST['Dance']: null);
+	$Soccer = (isset($_POST['Soccer']) ? $_POST['Soccer']: null);
+	$WeightLifting = (isset($_POST['Weight Lifting']) ? $_POST['Weight Lifting']: null);
+	$Yoga = (isset($_POST['Yoga']) ? $_POST['Yoga']: null);
+	
+	unset($sql);
+	if($Aquatics) {
+		$sql[] = " = '$Aquatics' ";
+	}
+	
+	if($Boxing) {
+		$sql[] = " = '$Boxing' ";
+	}
+	
+	if($Cycling) {
+		$sql[] = " = '$Cycling' ";
+	}
+	
+	if($Dance) {
+		$sql[] = " = '$Dance' ";
+	}
+	
+	if($Soccer) {
+		$sql[] = " = '$Soccer' ";
+	}
+	
+	if($WeightLifting) {
+		$sql[] = " = '$WeightLifting' ";
+	}
+	
+	if($Yoga) {
+		$sql[] = " = '$Yoga' ";
+	}
+}
+
+$select = $_POST['formClass'];
+
+$sql1 = "SELECT * FROM trainer";
+$sql2 = "SELECT T.* FROM trainer T WHERE T.employee_id IN(SELECT C.employee_id FROM class C";
+$sql3 = "SELECT * FROM class";
+$sql4 = "SELECT * FROM membership";
+$sql5 = "SELECT * FROM member";
+
+if(!empty($sql)) {
+	$sql3 .= ' WHERE ' .implode(' C.type ', $sql);
+	$sql1 = $sql3;
+}
+
+$query = mysqli_query($conn, $sql1);
+$query2 = mysqli_query($conn, $sql3);
+$query3 = mysqli_query($conn, $sql4);
+$query4 = mysqli_query($conn, $sql5);
 
 if (!$query) {
 	die ('SQL Error: ' . mysqli_error($conn));
 }
 ?>
 <html>
+<p>
+<h1>Trainer by Class Selector</h1>
+<center><form action="" method="post">
+<select name="formClass">
+	<option value="">Select...</option>
+	<option value="Aquatics">Aquatics</option>
+	<option value="Boxing">Boxing</option>
+	<option value="Cycling">Cycling</option>
+	<option value="Dance">Dance</option>
+	<option value="Soccer">Soccer</option>
+	<option value="WeightLifting">Weight Lifting</option>
+	<option value="Yoga">Yoga</option>
+</select>
+<input type="submit" name="submit" value="Find"/>
+</form></center>
+</p>
 <head>
 	<title>Displaying MySQL Data in HTML Table</title>
 	<style type="text/css">
@@ -110,6 +181,8 @@ if (!$query) {
 				<th>First Name</th>
 				<th>Last Name</th>
 				<th>Certification</th>
+				<th>Email</th>
+				<th>Phone</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -123,6 +196,91 @@ if (!$query) {
 					<td>'.$row['first_name'].'</td>
 					<td>'.$row['last_name'].'</td>
 					<td>'.$row['certification'].'</td>
+					<td>'.$row['email'].'</td>
+					<td>'.$row['phone'].'</td>
+				</tr>';
+			$no++;
+		}?>
+		</tbody>
+	</table>
+	<h1>Classes</h1>
+	<table class="data-table">
+		<thead>
+			<tr>
+				<th>Class ID</th>
+				<th>Class Name</th>
+				<th>Room</th>
+				<th>Type</th>
+				<th>Employee ID</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		$no 	= 1;
+		$total 	= 0;
+		while ($row = mysqli_fetch_array($query2))
+		{
+			echo '<tr>
+					<td>'.$no.'</td>
+					<td>'.$row['name'].'</td>
+					<td>'.$row['room'].'</td>
+					<td>'.$row['type'].'</td>
+					<td>'.$row['employee_id'].'</td>
+				</tr>';
+			$no++;
+		}?>
+		</tbody>
+	</table>
+	<h1>Memberships</h1>
+	<table class="data-table">
+		<thead>
+			<tr>
+				<th>Membership ID</th>
+				<th>Type</th>
+				<th>Price</th>
+				<th>Duration</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		$no 	= 1;
+		$total 	= 0;
+		while ($row = mysqli_fetch_array($query3))
+		{
+			echo '<tr>
+					<td>'.$no.'</td>
+					<td>'.$row['type'].'</td>
+					<td>'.$row['price'].'</td>
+					<td>'.$row['duration'].'</td>
+				</tr>';
+			$no++;
+		}?>
+		</tbody>
+	</table>
+	<table>
+	<h1>Members</h1>
+	<table class="data-table">
+		<thead>
+			<tr>
+				<th>Membership ID</th>
+				<th>First Name</th>
+				<th>Last Name</th>
+				<th>Email</th>
+				<th>Phone</th>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		$no 	= 1;
+		$total 	= 0;
+		while ($row = mysqli_fetch_array($query4))
+		{
+			echo '<tr>
+					<td>'.$no.'</td>
+					<td>'.$row['first_name'].'</td>
+					<td>'.$row['last_name'].'</td>
+					<td>'.$row['email'].'</td>
+					<td>'.$row['phone'].'</td>
 				</tr>';
 			$no++;
 		}?>
